2bc223511218a3472c31c1374197d96b4de714df
Michael Williamson <mike@zwobble.org>
2014-01-05 23:08:07 +0000

    Split out some functions for clarity

--- MammothViewer/viewer.js
+++ MammothViewer/viewer.js
@@ -1,14 +1,31 @@
 ﻿(function() {
+    function main() {
+        initialiseFileChoosers();
+    }
+
     var templateFileChooser = document.getElementById("template-file-chooser").innerHTML;
-    Array.prototype.forEach.call(document.querySelectorAll(".file-chooser"), initialiseFileChooser);
+
+    function initialiseFileChoosers() {
+        Array.prototype.forEach.call(
+            document.querySelectorAll(".file-chooser"),
+            initialiseFileChooser
+        );
+    }
+
     function initialiseFileChooser(element) {
-        var fragment = document.createElement("div");
-        fragment.innerHTML = templateFileChooser;
-        while (fragment.firstChild) {
-            element.appendChild(fragment.firstChild);
-        }
+        appendHtml(element, templateFileChooser);
         element.querySelector(".action").addEventListener("click", function() {
             element.querySelector(".file-path").value = MammothViewer.openFile();
         });
     }
+
+    function appendHtml(element, html) {
+        var fragment = document.createElement("div");
+        fragment.innerHTML = html;
+        while (fragment.firstChild) {
+            element.appendChild(fragment.firstChild);
+        }
+    }
+
+    main();
 })();
