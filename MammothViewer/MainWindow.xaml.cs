21aa4ad38083f257eedf95f08b9d0efd2f4c0f19
Michael Williamson <mike@zwobble.org>
2014-01-06 00:26:42 +0000

    Hack together working prototype

--- MammothViewer/MainWindow.xaml.cs
+++ MammothViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using CefSharp;
+using CefSharp.Wpf;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,7 +18,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MammothViewer
 {
@@ -25,7 +27,7 @@ namespace MammothViewer
         {
             var settings = new CefSharp.Settings
             {
-                PackLoadingDisabled = true,
+                PackLoadingDisabled = false,
             };
             CEF.Initialize(settings);
             InitializeComponent();
@@ -42,8 +44,9 @@ namespace MammothViewer
 
         private void OnBrowserInitialised()
         {
-            WebView.RegisterJsObject("MammothViewer", new MammothViewerModel());
+            WebView.RegisterJsObject("MammothViewerBackend", new MammothViewerModel(WebView));
             WebView.LoadHtml(ReadResource("MammothViewer.index.html"));
+            WebView.ShowDevTools();
         }
 
         private static string ReadResource(string resourceName)
@@ -60,8 +63,22 @@ namespace MammothViewer
 
         private class MammothViewerModel
         {
+            private readonly WebView m_Browser;
+            private IList<FileSystemWatcher> m_FileWatchers = new List<FileSystemWatcher>();
+
             public string Styles { get { return ReadResource("MammothViewer.style.css"); } }
-            public string Js { get { return ReadResource("MammothViewer.viewer.js"); } }
+            public string Js { get {
+                return ReadResource("MammothViewer.mammoth.js") + ReadResource("MammothViewer.viewer.js");
+            } }
+
+            public MammothViewerModel(WebView browser)
+            {
+                m_Browser = browser;
+            }
+            public string ReadFile(string path)
+            {
+                return Convert.ToBase64String(File.ReadAllBytes(path));
+            }
 
             public string OpenFile()
             {
@@ -76,6 +93,38 @@ namespace MammothViewer
                 {
                     return null;
                 }
+            }
+
+            public void SetWatchedFiles(string watchedFilesJson) {
+                var paths = DeserialiseJson<IList<string>>(watchedFilesJson);
+                foreach (var watcher in m_FileWatchers) {
+                    // TODO: disposal when application closes
+                    watcher.Dispose();
+                }
+                m_FileWatchers = paths
+                        .Where(File.Exists)
+                        .Select(WatchFile)
+                        .ToList();
+            }
+
+            private FileSystemWatcher WatchFile(string path)
+            {
+                var directory = Path.GetDirectoryName(path);
+                var filename = Path.GetFileName(path);
+                var watcher = new FileSystemWatcher(directory, filename);
+                watcher.Changed += (x, y) => RenderOutput();
+                return watcher;
+            }
+
+            private void RenderOutput()
+            {
+                m_Browser.ExecuteScript("MammothViewer.renderOutput();");
+            }
+
+            private T DeserialiseJson<T>(string json)
+            {
+                var serializer = new JavaScriptSerializer();
+                return serializer.Deserialize<T>(json);
             }
         }
     }
